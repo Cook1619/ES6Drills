c349e1593ea42aa945f5e78b42539365f2b42478
Cook1619 <cook6348@gmail.com>
2018-06-06 07:17:02 -0500

    Added notes, started the spread excercises

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ let favMovie = (movie = 'The Room', name = 'world') => console.log(`My name is $
 favMovie();
 
 let getFirstName = (fullName) => {
+    //spilt indentifies the space between the words and separates them into it's own index in an array
+    //then I grabb the first index [0] which would be my first name
     return fullName.split(' ')[0];
 }
 console.log(getFirstName('Matt Cook'));
@@ -18,9 +20,18 @@ console.log(getFirstName2('Matt Cook'))
 
 
     let newFunc = ((num1, num2) => ({
+        //squares the numbers
         squared: Math.pow(num1, num2),
+        //multplies the numbers together
         product: num1 * num2
     }
+    //First I tried calling the function normally, both came up undefined.
+    //Why do I need to use an IFFE to get this to work?
     ))(2,3);
     
 console.log(`${newFunc.squared} ${newFunc.product}`);
+
+let spreadFunc = (name, location, favFood) =>{
+
+} 
+let arr1 = ['Matt','Mayer','Mexican']
